49067a5962b82f8b804c139098a64cd8aab5347b
ddcodefreaks <developers.dynamic@gmail.com>
2023-10-23 13:34:37 +0000

    added lecture 6

--- streamapis/src/test/java/lectures/Lecture6.java
+++ streamapis/src/test/java/lectures/Lecture6.java
@@ -1,9 +1,9 @@
 package lectures;
 
 
-import com.google.common.collect.Lists;
 import java.util.Arrays;
 import java.util.function.Predicate;
+
 import org.junit.Test;
 
 public class Lecture6 {
@@ -13,13 +13,24 @@ public class Lecture6 {
   @Test
   public void findAny() throws Exception {
     Integer[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int number = Arrays.stream(numbers).filter(n-> n <10)
+    .findAny().orElse(-1);
+    //optimized way
+    int number1 = Arrays.stream(numbers).filter(numbersLessThan10)
+    .findAny().orElse(-1);
+    System.out.println("number::"+ number);
 
   }
 
   @Test
   public void findFirst() throws Exception {
     Integer[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-
+    int number = Arrays.stream(numbers).filter(n-> n <10)
+    .findFirst().orElse(-1);
+    //optimized way
+    int number1 = Arrays.stream(numbers).filter(numbersLessThan10)
+    .findFirst().orElse(-1);
+    System.out.println("numberFirst::"+ number);
   }
 }
 
