f727921bc4dac6696991e9495eb51723e07f0227
ddcodefreaks <developers.dynamic@gmail.com>
2023-10-23 13:10:11 +0000

    added code v2

--- streamapis/src/test/java/lectures/Lecture5.java
+++ streamapis/src/test/java/lectures/Lecture5.java
@@ -2,6 +2,7 @@ package lectures;
 
 
 import java.util.List;
+import java.util.OptionalDouble;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
@@ -11,6 +12,7 @@ import com.google.common.collect.ImmutableList;
 
 import beans.Car;
 import beans.Person;
+import beans.PersonDTO;
 import mockdata.MockData;
 
 public class Lecture5 {
@@ -21,13 +23,13 @@ public class Lecture5 {
     //filter cars based on the  price  less than  1000 
     List<Car> filteredCars = cars.stream().filter(car-> car.getPrice()<10000)
     .collect(Collectors.toList());
-    filteredCars.forEach(System.out::println);
+    //filteredCars.forEach(System.out::println);
 
     // filter car based on price but use predicate 
     Predicate<Car>  predicateCar =  car-> car.getPrice() <20000 ;
     List<Car> filteredCarswithPred = cars.stream().filter(predicateCar)
     .collect(Collectors.toList());
-    filteredCarswithPred.forEach(System.out::println);
+    //filteredCarswithPred.forEach(System.out::println);
     
 
   }
@@ -36,18 +38,33 @@ public class Lecture5 {
   public void ourFirstMapping() throws Exception {
     // transform from one data type to another
     List<Person> people = MockData.getPeople();
+    // transform Person class into PersonDTO
+    List<PersonDTO> dtos = people.stream().map(person -> { 
+    PersonDTO personDto = new PersonDTO(person.getId(), 
+    person.getFirstName(), person.getAge());
+    return personDto;
+    }).collect(Collectors.toList());
+    //dtos.forEach(System.out::println);;
 
+    // transform using the  map predicate
+    List<PersonDTO> dtos1 = people.stream().map(PersonDTO::map).collect(Collectors.toList());
+    dtos1.forEach(System.out::println);;
   }
 
   @Test
   public void averageCarPrice() throws Exception {
+    ImmutableList<Car> cars = MockData.getCars();
     // calculate average of car prices
+    OptionalDouble averageCarprice = cars.stream()
+    .mapToDouble(car-> car.getPrice()).average();
+    //System.out.println("average care price::"+ averageCarprice.getAsDouble());
 
+    // optimized way
+    double average = cars.stream().mapToDouble(Car::getPrice).average().orElse(0);
+    System.out.println("average care price::"+ average);
   }
-
   @Test
   public void test() throws Exception {
-
   }
 }
 
