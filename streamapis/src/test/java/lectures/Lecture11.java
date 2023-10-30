0f289770ab4567d7b1903b69da68975a012d8076
dev_bhaskar2b <developers.dynamic@gmail.com>
2023-10-31 01:19:36 +0530

    added lecture 11

--- streamapis/src/test/java/lectures/Lecture11.java
+++ streamapis/src/test/java/lectures/Lecture11.java
@@ -2,6 +2,7 @@ package lectures;
 
 import com.google.common.collect.ImmutableList;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import mockdata.MockData;
 import org.junit.Test;
@@ -11,12 +12,23 @@ public class Lecture11 {
   @Test
   public void joiningStrings() throws Exception {
     List<String> names = ImmutableList.of("anna", "john", "marcos", "helena", "yasmin");
-
+   String   joinedName = "" ;
+    for(String name : names)
+    {
+    	joinedName = joinedName + ", " + name;
+    }
+    //removing first comma
+    joinedName = joinedName.substring(2, joinedName.length()-1);
+    System.out.println(joinedName);
+    
   }
 
   @Test
-  public void joiningStringsWithStream() throws Exception {
+  public void joiningStringsWithStream() throws Exception
+  {
     List<String> names = ImmutableList.of("anna", "john", "marcos", "helena", "yasmin");
-
+    //using join
+    String join = names.stream().map(String :: toUpperCase).collect(Collectors.joining("|"));
+    System.out.println(join);
   }
 }
